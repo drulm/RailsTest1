30a37f60070a8a99ad03a77964b88c97e53873c2
Darrell Ulm <darrellulm@gmail.com>
2017-04-27 19:26:11 -0400

    db:migrate after Comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170421211850) do
+ActiveRecord::Schema.define(version: 20170427232057) do
 
   create_table "articles", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
     t.string   "title"
@@ -19,4 +19,14 @@ ActiveRecord::Schema.define(version: 20170421211850) do
     t.datetime "updated_at",               null: false
   end
 
+  create_table "comments", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
+    t.string   "commenter"
+    t.text     "body",       limit: 65535
+    t.integer  "article_id"
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+    t.index ["article_id"], name: "index_comments_on_article_id", using: :btree
+  end
+
+  add_foreign_key "comments", "articles"
 end
